652d1d52c1dd9b84ef12fd534a33a81cfb6bdf69
Kurt Keller <git+grab_number@pinboard.jp>
2018-03-23 18:59:40 +0100

    change logging during lookup to include ID
    Each number looked up gets an unique ID so the
    lines in the log belonging together can more
    easily be identified (with the ID= parameter)

--- funcs/lookup.py
+++ funcs/lookup.py
@@ -1,6 +1,8 @@
 import time
 import feedparser
 import urllib
+import random
+import sha
 from .logging import *
 from .caching import *
 
@@ -40,11 +42,17 @@ def lookup(C, di_cache, question):
   """
 
   # ----------------------------------------------------------------------
+  # create a unique ID for this request to follow it though the log
+  ID=sha.sha(repr(random.random())).hexdigest()
+
+  log(C, severity="I", msg='ID=%s question="%s"' % (ID, question))
+
+  # ----------------------------------------------------------------------
   # first check the cache
   if question in di_cache:
     if di_cache[question]["last_update"] > int(time.time()) - C.CacheAge:
-      log(C, severity="I", msg='question="%s" location=%s answer="%s"' % (
-                question, "cache", di_cache[question]["title"]))
+      log(C, severity="I", msg='ID=%s location=%s answer="%s"' % (
+                ID, "cache_positive", di_cache[question]["title"]))
       return (di_cache, di_cache[question]["title"])
 
   # if we do have an APIKey, then use it (create a dict to merge), if we
@@ -60,14 +68,15 @@ def lookup(C, di_cache, question):
 # KK: python3: { **{"was": question, "maxnum": 1}, **di_APIKey }
 
   if rss.status != 200:
-    log(C, severity="W", msg='status=%s msg="lookup unsuccessful"' % rss.status)
+    log(C, severity="W", msg='ID=%s status=%s msg="lookup unsuccessful"' % (
+                            ID, rss.status))
     if question in di_cache:
-      log(C, severity="I", msg='question="%s" location=%s answer="%s"' % (
-                question, "cache_expired", di_cache[question]["title"]))
+      log(C, severity="I", msg='ID="%s" location=%s answer="%s"' % (
+                ID, "cache_expired", di_cache[question]["title"]))
       return (di_cache, di_cache[question]["title"])
     else:
-      log(C, severity="I", msg='question="%s" location=%s answer="%s"' % (
-                question, "lookup_failed", question))
+      log(C, severity="I", msg='ID=%s location=%s answer="%s"' % (
+                ID, "lookup_failed", question))
       return (di_cache, question)
 
   if C.DEBUG:
@@ -78,15 +87,15 @@ def lookup(C, di_cache, question):
         log(C, severity="D", msg="entry=%d %s=%s" % (entrynum, key, entry[key]))
 
   try:
-    log(C, severity="I", msg='question="%s" location=%s answer="%s"' % (
-              question, "lookup_succeeded", rss.entries[0].title))
+    log(C, severity="I", msg='ID=%s location=%s answer="%s"' % (
+              ID, "lookup_succeeded", rss.entries[0].title))
     di_cache[question] = {"title": rss.entries[0].title,
                           "last_update": int(time.time())}
     save_cache(C, di_cache)
     return (di_cache, rss.entries[0].title)
   except:
-    log(C, severity="W", msg='msg="lookup successful but result not parsable"')
-    log(C, severity="I", msg='question="%s" location=%s answer="%s"' % (
-              question, "lookup_failed", question))
+    log(C, severity="W", msg='ID=%s msg="lookup successful but result not parsable"' % ID)
+    log(C, severity="I", msg='ID=%s location=%s answer="%s"' % (
+              ID, "lookup_failed", question))
     return (di_cache, question)
 
